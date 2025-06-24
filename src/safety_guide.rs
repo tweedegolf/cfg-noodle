8d2d60704118e2e82bc623ab41eb9625bf1ba235
James Munns <james@onevariable.com>
2025-06-24 16:56:02 +0200

    Update safety docs to cover detach

--- src/safety_guide.rs
+++ src/safety_guide.rs
@@ -1,13 +1,18 @@
 //! # Implementor's safety guide
 //!
-//! This is only relevant if you are working on (or curious about) the internals of
-//! `cfg-noodle` - as a user, all safety invariants are maintained internally.
+//! This is only relevant if you are working on (or curious about) the internals
+//! of `cfg-noodle` - as a user, all safety invariants are maintained internally.
 //!
 //! ## The Safety Rules
 //!
 //! ### State Transitions
 //!
+//! Nodes begin in the `Initial` state.
+//!
 //! ```text
+//!             ─ ─ ─ ─ (6)
+//!            │
+//!            ▼
 //!       ┌─────────┐   (1)    ┌─────────────┐
 //!       │ Initial │─────────▶│ NonResident │
 //!       └─────────┘          └─────────────┘
@@ -37,19 +42,38 @@
 //! 5. ValidNoWriteNeeded -> NeedsWrite
 //!     * NODE ONLY TRANSITTION
 //!     * during `write()` call
+//! 6. (any state) -> Initial
+//!     * NODE ONLY TRANSITION
+//!     * during `detach()` call
 //!
 //! ### `StorageList` Rules
 //!
-//! 1. When adding (or removing) nodes, we MUST be holding the mutex.
-//! 2. When changing the state of a node, we MUST be holding the mutex (for the full operation, e.g. also when writing to T in transition #2).
-//! 3. The ONLY time the list may take a mut ref to the `t`, is while IN the Initial state, AND while holding the mutex
+//! 1. When attaching (or detaching) nodes, we MUST be holding the mutex.
+//! 2. When changing the state of a node, we MUST be holding the mutex (for the
+//!    full operation, e.g. also when writing to T in transition #2).
+//! 3. The ONLY time the list may take a mut ref to the `t`, is while IN the
+//!    Initial state, AND while holding the mutex
 //!
 //! ### `StorageListNode`/`StorageListNodeHandle` Rules
 //!
-//! 1. The Node header is ALWAYS shared, EXCEPT at the time of attach, at which point we will hold a mut ref to link to the list, this requires we hold the mutex.
-//! 2. A Node must be attached to zero or one lists:
-//!     * We must guarantee a node is prevented from being added to a second list, if it already exists in one.
-//! 3. `attach()` MUST NOT complete/we MUST NOT create a StorageListNodeHandle UNTIL we are no longer in the Initial state.
-//! 4. DURING `StorageListNode::attach()`, we may ONLY take a mut ref to `t` IF we are in the NonResident state, AND we hold the mutex.
-//! 5. When calling `StorageListNodeHandle::write()`, we MUST hold the mutex.
-//! 6. When calling `StorageListNodeHandle::load()`, we DO NOT need the mutex, because we've already guaranteed we are not in Initial/NonResident state, and the list will never attempt to mutate `t`, but may also have a shared ref to `t`.
+//! 1. The Node header is ALWAYS shared, EXCEPT at the time of attach or detach,
+//!    at which point we will hold a mut ref to (un)link to the list, this
+//!    requires we hold the mutex.
+//! 2. A Node must be attached to zero or one lists at one time:
+//!     * We must guarantee a node is prevented from being added to a second
+//!       list, if it already exists in one.
+//!     * Users MUST detach the node from the list it is in before attaching it
+//!       to a new list.
+//! 3. A Node must have zero or one handles live at one time:
+//!     * We must guarantee a handle is prevented from being created if the node
+//!       already has a handle live
+//!     * The previous handle MUST be dropped before we create a new handle
+//! 4. `attach()` MUST NOT complete/we MUST NOT create a StorageListNodeHandle
+//!    UNTIL we are no longer in the Initial state.
+//! 5. DURING `StorageListNode::attach()`, we may ONLY take a mut ref to `t` IF
+//!    we are in the NonResident state, AND we hold the mutex.
+//! 6. When calling `StorageListNodeHandle::write()`, we MUST hold the mutex.
+//! 7. When calling `StorageListNodeHandle::load()`, we DO NOT need the mutex,
+//!    because we've already guaranteed we are not in Initial/NonResident state,
+//!    and the list will never attempt to mutate `t`, but may also have a shared
+//!    ref to `t`.
